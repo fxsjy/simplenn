254c3b732bd5db1c37c28ee617754bef9b46f4b8
Sun Junyi <ccnusjy@gmail.com>
2012-10-31 10:27:45 +0800

    softmax smooth

--- example2.py
+++ example2.py
@@ -1,23 +1,28 @@
 import simplenn
 from math import *
+import random
+
 def demo_sin():
     X = []
     tmp= 0
-    for i in xrange(100):
+    for i in xrange(200):
         X.append(tmp)
-        tmp+=pi/99
+        tmp+=2*pi/199
     old_x = X[:]
-    Y = [sin(x) for x in X]
+    Y = [sin(x)+random.random()*0.1 for x in X]
     old_y = Y[:]
 
     X = [{'I':x} for x in X]
     Y = [{'O':y} for y in Y]
 
-    nn = simplenn.RBFNN(['I'],['O'],nCenters=100)
+    nn = simplenn.RBFNN(['I'],['O'],nCenters=157)
 
     err = nn.train(X,Y,max_iter=1000)
 
     tmp = []
+
+   
+
     for x in X:
         tmp.append(nn.test_onece(x)['O'])
    
--- simplenn/core.py
+++ simplenn/core.py
@@ -1,13 +1,13 @@
 import random
 import math
-import pprint
+#import pprint
 
 def sigmoid(x):
-	return math.tanh(x)
+	return 1.0/(1 + math.e**(-x))
 
 
 def dsigmoid(y):
-	return 1.0 - y**2
+	return y*(1.0-y)
 
 
 class NN():
@@ -21,7 +21,7 @@ class NN():
 			self.weight_matix[ip] = dict([(op,-2.0+random.random()*4.0) for op in outputs_keys])
 			self.lastchange[ip] = dict([(op,0) for op in outputs_keys])
 
-	def train_onece(self, X,Y,sm,eta=0.01,beta=0.001):
+	def train_onece(self, X,Y,sm,eta=0.25,beta=0.1):
 		X['~']=1.0
 		if sm :
 			Y_ = dict([ (k, sigmoid(sum([ X[i]* self.weight_matix[i][k] for i in X.keys() ])) ) for k in Y.keys() ] )
@@ -29,23 +29,27 @@ class NN():
 			for k2 in Y:
 				dsm = dsigmoid(Y_[k2])
 				for k1 in X:
-					change = dsm* error[k2]*X[k1]
-					self.weight_matix[k1][k2] -= (change*eta+ self.lastchange[k1][k2]*beta)
+					delta = dsm* error[k2]*X[k1]
+					change = delta*eta+ self.lastchange[k1][k2]*beta
+					self.weight_matix[k1][k2] -= change
 					self.lastchange[k1][k2] = change
 		else:
 			Y_ = dict([ (k, sum([ X[i]* self.weight_matix[i][k] for i in X.keys() ]) ) for k in Y.keys() ] )
 			error = dict([(k,Y_[k]-v) for k,v in Y.iteritems()])
 			for k1 in X:
 				for k2 in Y:
-					change = error[k2]*X[k1]
-					self.weight_matix[k1][k2] -= ( change*eta + self.lastchange[k1][k2]*beta)
+					delta = error[k2]*X[k1]
+					change = delta*eta + self.lastchange[k1][k2]*beta
+					self.weight_matix[k1][k2] -= change
 					self.lastchange[k1][k2] = change
 		return 0.5*sum([i**2 for i in error.values()])
 
 	def train(self,inputs,outputs,sm=False):
 		err = 0.0
-		eta = 1.0/len(inputs)
-		beta = 0.1/len(inputs)
+		if sm==False:
+			eta,beta = 0.025,0.01
+		else:
+			eta,beta = 0.25,0.1
 		for i, x in enumerate(inputs):
 			y = outputs[i]
 			err += self.train_onece(x,y,sm,eta=eta,beta=beta)
@@ -71,31 +75,47 @@ class RBFNN():
 		k1_set = p.keys()
 		k2_set = c.keys()
 		union_set = set(k1_set) | set(k2_set)
-		dist = sum([ (p.get(k,0.0)-c.get(k,0.0))**2 for k in union_set])
-		return math.e ** (-8*dist)
+		dist = sum([ ( p.get(k,0.0)-c.get(k,0.0) )**2 for k in union_set])
+		return math.exp(-8*dist)
 
 	def train(self,inputs,outputs,max_iter=1000,sm=False):
 		new_inputs = []
 		self.centers = [inputs[i] for i in self.center_idx_list]
 		for i,p in enumerate(inputs):
 			new_inputs.append({})
+			div = 0.0
 			for j,c in enumerate(self.centers):
 				new_inputs[i][j] = self.make_rbf(p,c)
+				div += new_inputs[i][j] 
+			for j,c in enumerate(self.centers):
+				new_inputs[i][j] /= div
+		#print new_inputs
 		self.nn = NN(range(len(self.centers)),self.outputs_keys)
 		#pprint.pprint(new_inputs,open("ip.txt","wb"))
 		#.pprint(outputs,open("op.txt","wb"))
 		change = range(len(new_inputs))
+		old_err1 = 1.0
+		old_err2 = 2.0
 		for j in xrange(max_iter):
 			new_inputs = [new_inputs[i] for i in change]
 			outputs = [outputs[i] for i in change]
 			err = self.nn.train(new_inputs,outputs,sm=sm)
-			random.shuffle(change)
 			if j%100==0:
 				print 'loop:',j,'err',err
+				if (old_err2-old_err1<0.001) and (old_err1-err<0.001):
+					break #early stopping
+			random.shuffle(change)
+			old_err2 = old_err1
+			old_err1 = err
 
 	def test_onece(self, X,sm=False):
 		d={}
+		div = 0.0
 		for j,c in enumerate(self.centers):
 			d[j] = self.make_rbf(X,c)
+			div += d[j]
+		for j,c in enumerate(self.centers):
+			d[j] /= div
 		Y_ = self.nn.test_onece(d,sm)
+		#print Y_
 		return Y_
